89936cf195a0acfaf144768e6d48a7284e59455d
Kyle Whitaker <kylewhitaker51@gmail.com>
2018-01-12 14:34:06 -0500

    connect to test db and print names

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log('Hello, Kyle!');
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
+
+	assert.equal(null, err);
+	console.log('Successfully connected to MongoDB server!');
+
+	db.collection('names').find({}).toArray(function(err, docs) {
+		docs.forEach(function(doc) { console.log(doc.name); });
+		db.close();
+	});
+
+	console.log('called find...');
+
+});
